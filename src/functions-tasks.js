289313bafe0e9127e96da58979088c8131dfd681
Roman <mamontov.r1995@gmail.com>
2024-03-31 14:30:20 +0300

    feat: add retry fn

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -137,8 +137,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let attempt = attempts;
+    while (attempt > 0) {
+      try {
+        return func();
+      } catch (error) {
+        attempt -= 1;
+      }
+    }
+    return 'No more attempts';
+  };
 }
 
 /**
